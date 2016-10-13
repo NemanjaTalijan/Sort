7f6e1a5790c560e9eea8bd307baa12f33f1a4fc7
NemanjaTalijan <nemanja.talijan@gmail.com>
2016-10-13 15:37:59 +0200

    Wrong class call, added new metod
    New metod printRandArray()

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
-import Util
+from Util import Array
 import sys
 
-array1 = Util()
+array1 = Array()
 print("Please enter the array lenght:")
 len = sys.stdin.readline()
 array1.makeRandArray(len)
+array1.printRandArray()
 array1.sortArray()
 array1.printSortedRandArray()
